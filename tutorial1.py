386c0c68ba9d91525a3516f56175c2b5ec0ffe20
SHIRSHAK\shirshak <shirshakkandel@gmail.com>
2020-10-09 16:57:13 +0545

    3)Page navigation and clicking elements

--- tutorial1.py
+++ tutorial1.py
@@ -5,29 +5,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://techwithtim.net")
-print(driver.title)
-search = driver.find_element_by_name('s')
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
 
+link = driver.find_element_by_link_text("Python Programming")
+link.click()
 
 try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
-
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.LINK_TEXT, "Beginner Python Tutorials"))
     )
-    articles = main.find_elements_by_tag_name("article")
-    for article in articles:
-        header = article.find_element_by_class_name("entry-summary")
-        print(header.text)
+
+    element.click()
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.ID, "sow-button-19310003"))
+    )
+    element.click()
+    driver.back()
+    driver.forward()
 
 
 except:
-    driver.quit()
-
-finally:
     driver.quit()
